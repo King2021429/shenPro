c69c0c427aabca291b62afb70a01dc9036b596fa
shenyue <jiangfeng03@bilibili.com>
2024-12-12 21:03:05 +0800

    upgrade

--- app/shenyue/model/robot.go
+++ app/shenyue/model/robot.go
@@ -1,8 +1,8 @@
 package model
 
 const (
-	Title1 = "可以加班点餐喽"
-	Title2 = "加班点餐还有30min截止"
+	Title1 = "可以点加班餐喽"
+	Title2 = "加班餐还有30min截止点餐"
 	Title3 = "可以自由拿两个随机加班餐"
 	// Url1 神月订餐机器人
 	Url1 = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=daf43e42-b437-400f-9c32-b3f7bd6a58fb"
