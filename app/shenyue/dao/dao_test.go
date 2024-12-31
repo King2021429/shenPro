20dfd29027c05d6b049ccb12ed349755417d08eb
shenyue <jiangfeng03@bilibili.com>
2024-12-31 15:01:43 +0800

    test_upgrade

--- app/shenyue/dao/dao_test.go
+++ app/shenyue/dao/dao_test.go
@@ -2,14 +2,17 @@ package dao
 
 import (
 	"context"
+	"shenyue-gin/app/shenyue/configs"
 )
 
 var TestDao *Dao
 var TestCtx context.Context
 
 func init() {
-	//_ = flag.Set("conf", "../../../configs")
+	// 初始化配置文件
+	configs.InitConfig()
+	config := configs.GetConfig()
 	//初始化总的dao
-	TestDao = NewDao()
+	TestDao = NewDao(config)
 	TestCtx = context.Background()
 }
--- app/shenyue/service/service_test.go
+++ app/shenyue/service/service_test.go
@@ -2,14 +2,17 @@ package service
 
 import (
 	"context"
+	"shenyue-gin/app/shenyue/configs"
 )
 
 var TestSrv *Service
 var TestCtx context.Context
 
 func init() {
-	//_ = flag.Set("conf", "../../../configs")
+	// 初始化配置文件
+	configs.InitConfig()
+	config := configs.GetConfig()
 	//初始化总的dao
-	TestSrv = NewService()
+	TestSrv = NewService(config)
 	TestCtx = context.Background()
 }
